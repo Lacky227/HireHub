f1c68a20ddc3af86f08e9a491c491e6ec265c735
Lacky227 <tishko.artyr@gmail.com>
2025-09-01 20:08:21 +0300

    feat: add route to register page

--- web-ui/src/App.jsx
+++ web-ui/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import LandingPage from './pages/LandingPage.jsx';
+import RegisterPage from './pages/RegisterPage.jsx'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <Router>
           <Routes>
               <Route path="/" element={<LandingPage />} />
+              <Route path="/register" element={<RegisterPage />} />
           </Routes>
       </Router>
   );
