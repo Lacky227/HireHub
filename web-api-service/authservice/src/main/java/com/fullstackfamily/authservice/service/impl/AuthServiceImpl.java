8ca6c000ada26f2185db7666ee2dcbed177e4cb4
Lacky227 <tishko.artyr@gmail.com>
2025-09-08 23:53:59 +0300

    feat: add logic login and register

--- web-api-service/authservice/src/main/java/com/fullstackfamily/authservice/service/impl/AuthServiceImpl.java
+++ web-api-service/authservice/src/main/java/com/fullstackfamily/authservice/service/impl/AuthServiceImpl.java
@@ -3,19 +3,52 @@ package com.fullstackfamily.authservice.service.impl;
 import com.fullstackfamily.authservice.dto.LoginRequest;
 import com.fullstackfamily.authservice.dto.MessageResponse;
 import com.fullstackfamily.authservice.dto.RegisterRequest;
+import com.fullstackfamily.authservice.models.User;
+import com.fullstackfamily.authservice.repository.AuthRepository;
 import com.fullstackfamily.authservice.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@AllArgsConstructor
 public class AuthServiceImpl implements AuthService {
+    private final AuthRepository authRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public ResponseEntity<MessageResponse> login(LoginRequest loginRequest) {
-        return null;
+        Optional<User> user = authRepository.findByEmail(loginRequest.getEmail());
+        if (user.isEmpty()) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(new MessageResponse("Invalid email or password"));
+        } else if (!passwordEncoder.matches(loginRequest.getPassword(), user.get().getPassword())) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(new MessageResponse("Invalid email or password"));
+        }
+        return ResponseEntity.ok(new MessageResponse("Login successful"));
     }
 
     @Override
     public ResponseEntity<MessageResponse> register(RegisterRequest registerRequest) {
-        return null;
+        if (authRepository.existsByEmail(registerRequest.getEmail())) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(new MessageResponse("Email already exists"));
+        }
+        User user = new User();
+        user.setFirstName(registerRequest.getFirstName());
+        user.setLastName(registerRequest.getLastName());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setRole(registerRequest.getRole());
+        authRepository.save(user);
+        return ResponseEntity.ok(new MessageResponse("User registered successfully"));
     }
 }
