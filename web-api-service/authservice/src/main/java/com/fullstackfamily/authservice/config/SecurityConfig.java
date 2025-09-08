4d2e14858bfa158461f7e305c0d5628ae3189ff1
Lacky227 <tishko.artyr@gmail.com>
2025-09-09 00:20:44 +0300

    fix: rename bean filter chain

--- web-api-service/authservice/src/main/java/com/fullstackfamily/authservice/config/SecurityConfig.java
+++ web-api-service/authservice/src/main/java/com/fullstackfamily/authservice/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/api/**").permitAll();
