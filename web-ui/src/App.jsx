3e9c7007f7b293ae2e495a91a836672597558647
Lacky227 <tishko.artyr@gmail.com>
2025-09-13 12:43:04 +0300

    feat: add route to dashboard

--- web-ui/src/App.jsx
+++ web-ui/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import LandingPage from './pages/LandingPage.jsx';
 import RegisterPage from './pages/RegisterPage.jsx'
 import LoginPage from "./pages/LoginPage.jsx";
+import DashboardRouter from "./pages/dashboards/DashboardRouter.jsx";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
               <Route path="/" element={<LandingPage />} />
               <Route path="/register" element={<RegisterPage />} />
               <Route path="/login" element={<LoginPage />} />
+              <Route path="/dashboard" element={<DashboardRouter />} />
           </Routes>
       </Router>
   );
