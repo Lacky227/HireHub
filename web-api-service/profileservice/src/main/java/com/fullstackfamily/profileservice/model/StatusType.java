b4b69ba336cd3ecd10973984058612cd4aab787b
Lacky227 <tishko.artyr@gmail.com>
2025-09-18 23:58:24 +0300

    feat: enhance StatusType

--- web-api-service/profileservice/src/main/java/com/fullstackfamily/profileservice/model/StatusType.java
+++ web-api-service/profileservice/src/main/java/com/fullstackfamily/profileservice/model/StatusType.java
@@ -1,5 +1,19 @@
 package com.fullstackfamily.profileservice.model;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum StatusType {
-    ACTIVE, ARCHIVED
+    ACTIVE("active"),
+    ARCHIVED("archived");
+
+    private String value;
+    StatusType(String value) {
+        this.value = value;
+    }
+    public static Optional<StatusType> fromValue(String value) {
+        return Arrays.stream(StatusType.values())
+                .filter(s -> s.value.equals(value))
+                .findFirst();
+    }
 }
