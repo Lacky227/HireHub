d5d02fa94c9789e7b88febe07eea4040ca52c86e
Lacky227 <tishko.artyr@gmail.com>
2025-09-18 23:25:42 +0300

    feat: create dto class for update profile

---
+++ web-api-service/profileservice/src/main/java/com/fullstackfamily/profileservice/dto/UpdateProfileRequest.java
@@ -0,0 +1,34 @@
+package com.fullstackfamily.profileservice.dto;
+
+import com.fullstackfamily.profileservice.model.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class UpdateProfileRequest {
+    private String firstName;
+    private String lastName;
+    private String contactEmail;
+    private String contactPhone;
+
+    private String headLine;
+    private String location;
+    private String role;
+    private String aboutMe;
+
+    private List<WorkExperience> workExperiences = new ArrayList<>();
+    private List<Education> educations = new ArrayList<>();
+    private List<String> skills = new ArrayList<>();
+    private List<Language> languages = new ArrayList<>();
+
+    private BigDecimal expectedSalary;
+
+    private EmploymentType employmentType;
+
+    private String linkedinUrl;
+    private String githubUrl;
+    private String portfolioUrl;
+}
