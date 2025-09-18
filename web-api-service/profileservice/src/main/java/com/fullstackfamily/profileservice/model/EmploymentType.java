f2542677212217ecc248a5338956e62dd8e6286d
Lacky227 <tishko.artyr@gmail.com>
2025-09-18 23:31:48 +0300

    feat: related models and enhance EmploymentType

--- web-api-service/profileservice/src/main/java/com/fullstackfamily/profileservice/model/EmploymentType.java
+++ web-api-service/profileservice/src/main/java/com/fullstackfamily/profileservice/model/EmploymentType.java
@@ -1,8 +1,22 @@
 package com.fullstackfamily.profileservice.model;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum EmploymentType {
-    FULL_TIME,
-    PART_TIME,
-    INTERNSHIP,
-    CONTRACT
+    FULL_TIME("full-time"),
+    PART_TIME("part-time"),
+    INTERNSHIP("internship"),
+    CONTRACT("contract")
+
+    ;
+    private String value;
+    EmploymentType(String value) {
+        this.value = value;
+    }
+    public static Optional<EmploymentType> fromValue(String value) {
+        return Arrays.stream(EmploymentType.values())
+                .filter(e -> e.value.equals(value))
+                .findFirst();
+    }
 }
